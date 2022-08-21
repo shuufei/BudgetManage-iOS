9b7e1c21658500b8347d7419b29f63d71af0d315
shuufei <digital.shuufei@gmail.com>
2022-08-21 12:50:14 +0900

    Initial Commit

---
+++ BudgetManage/BudgetManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetManageApp.swift
+//  BudgetManage
+//
+//  Created by shuuhei hanashiro on 2022/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BudgetManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetManage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BudgetManage
+//
+//  Created by shuuhei hanashiro on 2022/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
