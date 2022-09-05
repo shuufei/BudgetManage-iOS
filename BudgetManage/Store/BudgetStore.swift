35b3ae8f22118a2a9046f92492326a80115b6d00
shuufei <digital.shuufei@gmail.com>
2022-09-05 21:28:52 +0900

    refactor: ファイルベースでの永続化、ロード処理を共通化

--- BudgetManage/Store/BudgetStore.swift
+++ BudgetManage/Store/BudgetStore.swift
@@ -27,69 +27,11 @@ class BudgetStore: ObservableObject {
     }
     
     static func load() async throws -> [Budget] {
-        try await withCheckedThrowingContinuation { continuation in
-            load { result in
-                switch result {
-                case .failure(let error):
-                    continuation.resume(throwing: error)
-                case .success(let budgets):
-                    continuation.resume(returning: budgets)
-                }
-            }
-        }
-    }
-    
-    static func load(completion: @escaping (Result<[Budget], Error>) -> Void) {
-        DispatchQueue.global(qos: .background).async {
-            do {
-                let fileURL = try fileURL()
-                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
-                    DispatchQueue.main.async {
-                        completion(.success([]))
-                    }
-                    return
-                }
-                let budgets = try JSONDecoder().decode([Budget].self, from: file.availableData)
-                DispatchQueue.main.async {
-                    completion(.success(budgets))
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-            }
-        }
+        try await FileBaseStore.load(fileURL: self.fileURL)
     }
     
     @discardableResult
     static func save(budgets: [Budget]) async throws -> Int {
-        try await withCheckedThrowingContinuation { continuation in
-            save(budgets: budgets) { result in
-                switch result {
-                case .failure(let error):
-                    continuation.resume(throwing: error)
-                case .success(let budgetsSaved):
-                    continuation.resume(returning: budgetsSaved)
-                }
-                
-            }
-        }
-    }
-    
-    static func save(budgets: [Budget], completion: @escaping (Result<Int, Error>) -> Void) {
-        DispatchQueue.global(qos: .background).async {
-            do {
-                let data = try JSONEncoder().encode(budgets)
-                let outfile = try fileURL()
-                try data.write(to: outfile)
-                DispatchQueue.main.async {
-                    completion(.success(budgets.count))
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-            }
-        }
+        try await FileBaseStore.save(budgets: budgets, fileURL: self.fileURL)
     }
 }
