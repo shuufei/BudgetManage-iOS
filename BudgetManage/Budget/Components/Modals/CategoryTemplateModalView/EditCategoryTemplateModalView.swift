8c3dd9bbf97791ed55e26b50180690f1f59a0094
shuufei <digital.shuufei@gmail.com>
2023-01-06 17:59:43 +0900

    feat: カテゴリ編集ダイアログに変更破棄確認ダイアログ対応を追加

--- BudgetManage/Budget/Components/Modals/CategoryTemplateModalView/EditCategoryTemplateModalView.swift
+++ BudgetManage/Budget/Components/Modals/CategoryTemplateModalView/EditCategoryTemplateModalView.swift
@@ -13,45 +13,45 @@ struct EditCategoryTemplateModalView: View {
 
     var categoryTemplateId: UUID
     
+    @State private var current: CategoryTemplate = CategoryTemplate(title: "", theme: .yellow)
     @State private var data: CategoryTemplate = CategoryTemplate(title: "", theme: .yellow)
     @State private var initialized: Bool = false
     
+    @State private var presentingConfirmationDialog: Bool = false
+    private var isModified: Bool {
+        get {
+            self.current != self.data
+        }
+    }
+    
+    private func commit() {
+        if let index = self.categoryTemplateStore.categories.firstIndex(where: { $0.id == self.categoryTemplateId }) {
+            self.categoryTemplateStore.categories[index] = self.data
+        }
+        self.dismiss()
+    }
+    
     var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    TextField("カテゴリ名", text: self.$data.title)
-                        .modifier(TextFieldClearButton(text: self.$data.title))
-                    ThemePicker(selection: self.$data.theme)
-                }
-                .onAppear {
-                    if self.initialized {
-                        return
-                    }
-                    if let categoryTemplate = self.categoryTemplateStore.categories.first(where: { $0.id == self.categoryTemplateId }) {
-                        self.data = categoryTemplate
-                        self.initialized = true
-                    }
-                }
+        List {
+            Section {
+                TextField("カテゴリ名", text: self.$data.title)
+                    .modifier(TextFieldClearButton(text: self.$data.title))
+                ThemePicker(selection: self.$data.theme)
             }
-            .navigationTitle("カテゴリの編集")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .cancellationAction) {
-                    Button("キャンセル") {
-                        self.dismiss()
-                    }
+            .onAppear {
+                if self.initialized {
+                    return
                 }
-                ToolbarItem(placement: .primaryAction) {
-                    Button("保存") {
-                        if let index = self.categoryTemplateStore.categories.firstIndex(where: { $0.id == self.categoryTemplateId }) {
-                            self.categoryTemplateStore.categories[index] = self.data
-                        }
-                        self.dismiss()
-                    }
+                if let categoryTemplate = self.categoryTemplateStore.categories.first(where: { $0.id == self.categoryTemplateId }) {
+                    self.current = categoryTemplate
+                    self.data = categoryTemplate
+                    self.initialized = true
                 }
             }
         }
+        .navigationTitle("カテゴリの編集")
+        .navigationBarTitleDisplayMode(.inline)
+        .confirmationDialog(isModified: self.isModified, onCommit: self.commit)
     }
 }
 
